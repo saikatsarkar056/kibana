8d1a313f25d44337bf702f6d7d06b64ccad6e6a7
Saikat Sarkar <saikat.sarkar@elastic.co>
2024-03-24 21:23:22 -0600

    Populate the reference_field dropdown by text fields

--- x-pack/plugins/index_management/public/application/components/mappings_editor/components/document_fields/fields/create_field/required_parameters_forms/semantic_text_type.tsx
+++ x-pack/plugins/index_management/public/application/components/mappings_editor/components/document_fields/fields/create_field/required_parameters_forms/semantic_text_type.tsx
@@ -6,24 +6,36 @@
  */
 
 import { i18n } from '@kbn/i18n';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import { useComponentTemplatesContext } from '../../../../../../component_templates/component_templates_context';
 import { getFieldConfig } from '../../../../../lib';
 import { FormRow, UseField } from '../../../../../shared_imports';
 import { SuperSelectOption } from '../../../../../types';
 import { ModelIdSelects } from '../../../field_parameters/model_id_selects';
+import { useLoadIndexMappings } from '../../../../../../../services';
 
 const fieldConfigReferenceField = getFieldConfig('reference_field');
 const fieldConfigModelId = getFieldConfig('model_id');
 
-const referenceFieldOptions: SuperSelectOption[] = [
-  { value: 'body-content', inputDisplay: 'body-content' },
-  { value: 'description', inputDisplay: 'description' },
-];
-
 export const SemanticTextRequiredParameters = () => {
   const { api } = useComponentTemplatesContext();
   const [inferenceModels, setInferenceModels] = useState<any>([]);
+
+  const location = useLocation();
+  const { search } = location;
+  const queryParams = useMemo(() => new URLSearchParams(search), [search]);
+  const indexName = queryParams.get('indexName') ?? '';
+  const { data } = useLoadIndexMappings(indexName);
+  const referenceFieldOptions: SuperSelectOption[] = [];
+  if (data && data.mappings && data.mappings.properties) {
+    Object.keys(data.mappings.properties).forEach((key) => {
+      const field = data.mappings.properties[key];
+      if (field.type === 'text') {
+        referenceFieldOptions.push({ value: key, inputDisplay: key });
+      }
+    });
+  }
 
   useEffect(() => {
     const fetchInferenceModels = async () => {
